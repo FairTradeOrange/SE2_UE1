bc0107be8614afbcf5f8f83209ba91f0e9f00a50
ThePieIsALie <FairTradeOrange@users.noreply.github.com>
2024-03-09 15:51:35 +0100

    -UI erstellt. - UI-Objekte in HomeFragment.java hinzugefügt.

--- app/src/main/java/se2/a1/softwareengineering_einzelarbeit/ui/home/HomeFragment.java
+++ app/src/main/java/se2/a1/softwareengineering_einzelarbeit/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,6 +17,8 @@ import se2.a1.softwareengineering_einzelarbeit.databinding.FragmentHomeBinding;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private Button server_send;
+    private EditText matrikel_number;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,10 +28,16 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        server_send = binding.buttonHomefragMatrikelnummer;
+        matrikel_number = binding.edittnHomefragMatrikelnummer;
+
+        final TextView textView_Matrikelnummer = binding.tvHomefragMatrikelnummer;
+        final TextView textView_Serverantwort = binding.tvHomefragAntwort;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView_Serverantwort::setText);
         return root;
     }
+
+
 
     @Override
     public void onDestroyView() {
